5419d7019ac832d3de06eeb38895b2b69ce7d9c1
Priyesh Shah <priyesh.shah147@gmail.com>
2021-05-28 16:14:59 -0400

    Controller

--- skeleton /app/controllers/users_controller.rb
+++ skeleton /app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
 
 
-    def new
+    def new 
+        @user = User.new
+        render :new
     end 
+
+    def create  
+        @user = User.new(params[:id])
+        if @user.save
+            redirect_to user_url(@user)
+        else
+            render :new
+        end
+
+    end
+
+
+    def user_params
+        params.require(:user).permits(:user_name, :password)
+    end
 end 
